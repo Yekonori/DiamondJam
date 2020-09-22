4323884145c5e3b476175645fd17307263043ed9
Yekonori <yannisdemacedo@gmail.com>
2020-09-22 09:39:10 +0200

    Swipe Controller

---
+++ DiamonJam/Assets/Sprites/Swipe.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    private float swipeDistanceThreshold = 50;
+
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+
+    private void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.touches[0];
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    // Stockage du point de départ
+                    startPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    // Stockage du point de fin
+                    endPosition = touch.position;
+                    AnalyzeGesture(startPosition, endPosition);
+                    break;
+            }
+        }
+    }
+
+    private void AnalyzeGesture(Vector2 start, Vector2 end)
+    {
+        // Distance
+        if (Vector2.Distance(start, end) > swipeDistanceThreshold)
+        {
+            // Le mouvement est suffisamment ample
+            Debug.Log("StartPosition : " + startPosition + " --- EndPosition : " + endPosition);
+        }
+    }
+}
