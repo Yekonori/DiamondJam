8c73a2cadc43b24ea42a31ce9f49b8e76ce8526d
Nam <nam.mirbeau@gmail.com>
2020-09-22 17:23:53 +0200

    Structure de donné pour les dialogues des personnages

--- DiamonJam/Assets/Scripts/ScriptableObjects/SO_CharacterData.cs
+++ DiamonJam/Assets/Scripts/ScriptableObjects/SO_CharacterData.cs
@@ -2,6 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+[System.Serializable]
+public class QuestionData
+{
+	[SerializeField]
+	private string questionID;
+	public string QuestionID
+	{
+		get { return questionID; }
+	}
+
+	[SerializeField]
+	private string questionTag;
+	public string QuestionTag
+	{
+		get { return questionTag; }
+	}
+
+	[SerializeField]
+	private int questionDifficulty;
+	public int QuestionDifficulty
+	{
+		get { return questionDifficulty; }
+	}
+}
+
+
+
+
+[System.Serializable]
+public class DiscussionData
+{
+	[SerializeField]
+	private string discussionID;
+	public string DiscussionID
+	{
+		get { return discussionID; }
+	}
+
+	[SerializeField]
+	private string discussionTag;
+	public string DiscussionTag
+	{
+		get { return discussionTag; }
+	}
+}
+
+[System.Serializable]
+public class DiscussionCharacterData
+{
+	[SerializeField]
+	private SO_CharacterData characterData;
+	public SO_CharacterData CharacterData
+	{
+		get { return characterData; }
+	}
+
+	[SerializeField]
+	private string discussionID;
+	public string DiscussionID
+	{
+		get { return discussionID; }
+	}
+
+	[SerializeField]
+	private string discussionTag;
+	public string DiscussionTag
+	{
+		get { return discussionTag; }
+	}
+}
+
+
+
 [System.Serializable]
 [CreateAssetMenu(fileName = "CharacterData", menuName = "Dialogue/CharacterData", order = 1)]
 public class SO_CharacterData : ScriptableObject
@@ -14,9 +89,45 @@ public class SO_CharacterData : ScriptableObject
 	}
 
 	[SerializeField]
-	private Sprite[] characterSprites;
-	public Sprite[] CharacterSprites
+	private GameObject characterModel;
+	public GameObject CharacterModel
 	{
-		get { return characterSprites; }
+		get { return characterModel; }
 	}
+
+	[TextArea()]
+	[SerializeField]
+	private string characterDescription;
+	public string CharacterDescription
+	{
+		get { return characterDescription; }
+	}
+
+
+	//[Space]
+	//[Space]
+	//[Space]
+	[Header("Discussion du personnage quand le joueur lui demande de parler de lui-même")]
+	[SerializeField]
+	private DiscussionData[] discussionDatas;
+	public DiscussionData[] DiscussionDatas { get; set; }
+
+
+
+	[Header("Discussion du personnage quand le joueur lui demande de parler d'un autre perso")]
+	[SerializeField]
+	private DiscussionCharacterData[] discussionCharacterDatas;
+	public DiscussionCharacterData[] DiscussionCharacterDatas { get; set; }
+
+
+	[Space]
+	[Space]
+	[Space]
+
+	[Header("Questions posé si le joueur porte le masque de ce personnage\n" +
+			" - Questions ID est l'identifiant du dialogue\n" +
+			" - Questions Tag est le nom de la variable pour sélectionner en priorité cette question")]
+	[SerializeField]
+	private QuestionData[] questionDatas;
+	public QuestionData[] QuestionDatas { get; set; }
 }
