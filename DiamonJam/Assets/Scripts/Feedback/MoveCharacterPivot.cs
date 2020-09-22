23f8709d1499a33cb5e5a403e56e704e9a402339
Nam <nam.mirbeau@gmail.com>
2020-09-22 10:36:48 +0200

    Scene Nam

---
+++ DiamonJam/Assets/Scripts/Feedback/MoveCharacterPivot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacterPivot : MonoBehaviour
+{
+
+    [SerializeField]
+    float speedCoroutine;
+    [SerializeField]
+    Transform newPivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveToNewParent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void MoveToNewParent()
+    {
+        this.transform.SetParent(newPivot);
+        StartCoroutine(MoveCharacterCoroutine());
+    }
+
+    private IEnumerator MoveCharacterCoroutine()
+    {
+        Vector3 originPosition = Vector3.zero;
+        Quaternion zero = Quaternion.Euler(0, 0, 0);
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / speedCoroutine;
+            this.transform.localRotation = Quaternion.Slerp(this.transform.localRotation, zero, t);
+            this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, originPosition, t);
+            yield return null;
+        }
+    }
+}
