c5d9462069634bda19665f1c59fb79d12607291b
Alexis <alexis.sainsard@gmail.com>
2020-09-22 10:12:20 +0200

    Add the main menu script

---
+++ DiamonJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+
+    public void StartGame()
+    {
+        // Need name of the scene or number
+        // SceneManager.LoadScene("MainScene");
+        Debug.Log("StartGame");
+    }
+
+    public void OpenMain()
+    {
+        mainPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+
+    public void OpenOptions()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+    }
+}
