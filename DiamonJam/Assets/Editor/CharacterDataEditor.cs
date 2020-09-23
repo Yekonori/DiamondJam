300407cb7193b4bc40fdba69a79b41253f5d32b1
Nam <nam.mirbeau@gmail.com>
2020-09-23 13:20:45 +0200

    Game Manager

--- DiamonJam/Assets/Editor/CharacterDataEditor.cs
+++ DiamonJam/Assets/Editor/CharacterDataEditor.cs
@@ -27,14 +27,14 @@ public class QuestionDataDrawer : PropertyDrawer
         EditorGUI.indentLevel = 0;
 
         // Calculate rects
-        var amountRect = new Rect(position.x, position.y, position.width * 0.38f, position.height);
-        var unitRect = new Rect(position.x + position.width * 0.4f, position.y, position.width * 0.58f, position.height);
-        //var nameRect = new Rect(position.x + position.width * 0.6f, position.y, position.width * 0.39f, position.height);
+        var amountRect = new Rect(position.x, position.y, position.width * 0.32f, position.height);
+        var unitRect = new Rect(position.x + position.width * 0.33f, position.y, position.width * 0.32f, position.height);
+        var nameRect = new Rect(position.x + position.width * 0.66f, position.y, position.width * 0.32f, position.height);
 
         // Draw fields - passs GUIContent.none to each so they are drawn without labels
         EditorGUI.PropertyField(amountRect, property.FindPropertyRelative("questionDifficulty"), GUIContent.none);
         EditorGUI.PropertyField(unitRect, property.FindPropertyRelative("questionID"), GUIContent.none);
-        //EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("questionTag"), GUIContent.none);
+        EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("questionTag"), GUIContent.none);
 
         // Set indent back to what it was
         EditorGUI.indentLevel = indent;
@@ -46,7 +46,7 @@ public class QuestionDataDrawer : PropertyDrawer
 
 
 
-[CustomPropertyDrawer(typeof(DiscussionData))]
+/*[CustomPropertyDrawer(typeof(DiscussionData))]
 public class DiscussionDataDrawer : PropertyDrawer
 {
     // Draw the property inside the given rect
@@ -78,7 +78,7 @@ public class DiscussionDataDrawer : PropertyDrawer
 
         EditorGUI.EndProperty();
     }
-}
+}*/
 
 
 
@@ -100,14 +100,14 @@ public class DiscussionCharacterDataDrawer : PropertyDrawer
         EditorGUI.indentLevel = 0;
 
         // Calculate rects
-        var amountRect = new Rect(position.x, position.y, position.width * 0.38f, position.height);
-        var unitRect = new Rect(position.x + position.width * 0.4f, position.y, position.width * 0.58f, position.height);
-        //var nameRect = new Rect(position.x + position.width * 0.66f, position.y, position.width * 0.32f, position.height);
+        var amountRect = new Rect(position.x, position.y, position.width * 0.32f, position.height);
+        var unitRect = new Rect(position.x + position.width * 0.33f, position.y, position.width * 0.32f, position.height);
+        var nameRect = new Rect(position.x + position.width * 0.66f, position.y, position.width * 0.32f, position.height);
 
         // Draw fields - passs GUIContent.none to each so they are drawn without labels
         EditorGUI.PropertyField(amountRect, property.FindPropertyRelative("characterData"), GUIContent.none);
         EditorGUI.PropertyField(unitRect, property.FindPropertyRelative("discussionID"), GUIContent.none);
-        //EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("discussionTag"), GUIContent.none);
+        EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("discussionTag"), GUIContent.none);
 
         // Set indent back to what it was
         EditorGUI.indentLevel = indent;
