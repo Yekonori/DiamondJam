6b802ac9d636c3ac49006464c1dc313f6d1721d3
Alexis <alexis.sainsard@gmail.com>
2020-09-24 09:50:14 +0200

    Creation SoundManager + prefab Creation dossier Sounds + import de sons pour bateau

---
+++ DiamonJam/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource music;
+    public AudioSource knifeOut;
+    public AudioSource killNPC;
+
+    #region Singleton
+    private static SoundManager _instance = null;
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    void PlaySound(AudioSource audio)
+    {
+        audio.Play();
+    }
+}
